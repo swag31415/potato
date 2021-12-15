677a9ad890a6e146386347b5c80231044f49232b
swag31415 <kidswagatam18@gmail.com>
2021-12-14 23:27:39 -0500

    Added the python grammar

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 from lark import Lark
 
 with open('grammar.lark', 'r') as f:
-  potato = Lark(f.read(), start='code')
+  potato = Lark(f.read(), start='file_input')
 
-tree = potato.parse('with open("grammar.lark", "r") as f')
+tree = potato.parse('''def fun(x): x + sin(x)
+''')
 print(tree)
 print(tree.pretty())
